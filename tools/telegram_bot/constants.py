8ebd64b07ca3ccf6a8092a5f0176d4dd64dc5a8b
Junhua <wenjh1998@gmail.com>
2021-08-02 21:32:54 +0800

    Update endpt

--- tools/telegram_bot/constants.py
+++ tools/telegram_bot/constants.py
@@ -1,3 +1,3 @@
 TELEGRAM_GET_ENDPOINT = "https://api.telegram.org/bot{token}/getUpdates"
 TELEGRAM_SEND_ENDPOINT = "https://api.telegram.org/bot{token}/sendMessage?chat_id={group_id}&text={message}"
-GROUP_ID = ""GROUP_ID = "-1001573312711"
+GROUP_ID = ""GROUP_ID = "-1001573312711"GROUP_ID = "-1001573312711"
