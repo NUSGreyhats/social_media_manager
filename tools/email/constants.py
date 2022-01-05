7e56b5cbf11280c58e0fc1f10f3d50fd05894fed
Wen Junhua <wenjh1998@gmail.com>
2022-01-05 09:05:45 -0500

    fix: update constants

--- tools/email/constants.py
+++ tools/email/constants.py
@@ -1,21 +1,7 @@
 EMAIL_ADDRESS = r""
 PASSWORD = ""
-SEND_ADDRESS = "recruit@nusgreyhats.org"
-SUBJECT = "Application for Welcome CTF"
-SMTP_HOST = 'smtp.zoho.com'
+SEND_ADDRESS = ""
+SUBJECT = ""
+SMTP_HOST = ''
 SMTP_PORT = 465
-BODY = """Dear {name},
-
-Thank you for your interest in joining NUS Greyhats.
-
-Unfortunately, due to overwhelming applications this year, we regret to inform you that your application was unsuccessful.
-
-Applications are open on a yearly basis and we invite you to apply again in the next cycle. 
-
-In the meantime, if there are any cybersecurity topics you would like to discuss, we will be glad to hear from you.
-
-Hope to see you around!
-
-Thank you and Best regards,
-NUS Greyhats
-"""
+BODY = """"""
