d13947b5ffaf4dc25312a79475f7e5e27533e37a
Wen Junhua <wenjh1998@gmail.com>
2022-01-27 16:31:01 -0500

    feat: add email integration

--- __main__.py
+++ __main__.py
@@ -1,36 +1,154 @@
 """The frontend for the social media manager"""
+import logging
 import os
 import configparser
-from flask import Flask, render_template, request, redirect, flash, Response
+from flask import Flask, abort, render_template, request, redirect, flash, Response
 from werkzeug.utils import secure_filename
 
-from constants import ERROR_FLASH, FACEBOOK, IMAGE_KEY, INFO_FLASH, SOCIAL_MEDIA, HOME_PAGE, SELECT_AT_LEAST_ONE_SOCIAL_MEDIA, SUCCESS_MESSAGE, EMPTY_CONTENT, CONTENT_KEY, EMPTY_STRING, DEFAULT_CONFIG_FILE, TELEGRAM, TWITTER, UPLOAD_FOLDER 
-from tools import post_to_twitter
-from tools.post import post_to_facebook, post_to_telegram
+from constants import (
+    BODY_KEY,
+    CSV_KEY,
+    EMAIL_KEY,
+    ERROR_FLASH,
+    FACEBOOK,
+    IMAGE_KEY,
+    INFO_FLASH,
+    SOCIAL_MEDIA,
+    HOME_PAGE,
+    SELECT_AT_LEAST_ONE_SOCIAL_MEDIA,
+    SUBJECT_KEY,
+    SUCCESS_MESSAGE,
+    EMPTY_CONTENT,
+    CONTENT_KEY,
+    EMPTY_STRING,
+    DEFAULT_CONFIG_FILE,
+    TELEGRAM,
+    TWITTER,
+    EMAIL_PAGE,
+    UPLOAD_FOLDER,
+)
+from tools.post import post_to_facebook, post_to_telegram, post_to_twitter
+from tools.email.email_sc import EmailIntegration, create_email
+from tools.email import extract_csv
 
 
+# Set up logger
+logging.basicConfig(
+    format="%(levelname)s:%(message)s",
+    filename="server.log",
+    encoding="utf-8",
+    level=logging.DEBUG,
+)
+
+
+# Set up flask
 app = Flask(__name__)
 app.secret_key = os.urandom(128).hex()
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 credentials = None
+email_server = None
 func_dict = {
     FACEBOOK: post_to_facebook,
     TELEGRAM: post_to_telegram,
-
     # TWITTER: post_to_twitter,
 }
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/email", methods=["GET", "POST"])
+def email() -> Response:
+    """The page for mass sending email"""
+    if request.method == "GET":
+        return render_template("email.html")
+
+    # Ensure that the email server is set up.
+    if email_server is None:
+        logging.error("Email server not set up")
+        return abort(
+            500,
+            "Email server not set up, please contact the administrator / check configuration",
+        )
+
+    subject = request.form.get(SUBJECT_KEY, None)
+    body = request.form.get(BODY_KEY, None)
+    csv_file = request.files.get(CSV_KEY, None)
+
+    if None in (subject, body, csv_file):
+        logging.error("Missing fields")
+        flash(EMPTY_CONTENT, ERROR_FLASH)
+        return redirect(EMAIL_PAGE)
+
+    filepath = os.path.join(UPLOAD_FOLDER, secure_filename(csv_file.filename))
+    csv_file.save(filepath)
+
+    # Extract the CSV into a list of dictionaries
+    header, data = extract_csv(filepath)
+    logging.info(f"Extracted {len(data)} rows from {filepath} with header {header}")
+
+    if EMAIL_KEY not in header:
+        logging.info("CSV file does not contain an email column")
+        flash(f"{CSV_KEY} must contain a column named {EMAIL_KEY}", ERROR_FLASH)
+        os.remove(filepath)
+        return redirect(EMAIL_PAGE)
+
+    logging.info(f"Sending emails to {len(data)} recipients")
+    error_count = 0
+    for row in data:
+        email = row.get("email", None)
+        if email is None:
+            error_count += 1
+            continue
+
+        # Format the email and send
+        try:
+            content_subject = subject.format(**row)
+            content_body = body.format(**row)
+        except KeyError as e:
+            flash(f"{e} not found in {BODY_KEY}", ERROR_FLASH)
+            os.remove(filepath)
+            return redirect(EMAIL_PAGE)
+
+        mime_email = create_email(
+            credentials["email"]["from_email"], [email], content_subject, content_body
+        )
+
+        # Send the email
+        try:
+            email_server.send_email(email, mime_email)
+        except Exception as e:
+            logging.error(f"Failed to send email to {email}: {e}")
+            error_count += 1
+            continue
+
+        logging.info(
+            f"Sent email to {email} with subject: {content_subject} and body {content_body}"
+        )
+
+    if error_count > 0:
+        logging.info(
+            f"{error_count} emails were not sent due to invalid/missing email addresses"
+        )
+        flash(
+            f"{error_count} emails were not sent due to missing email addresses. Please check the logs",
+            ERROR_FLASH,
+        )
+    else:
+        logging.info(f"Sent {len(data)} emails")
+        flash(SUCCESS_MESSAGE, INFO_FLASH)
+    os.remove(filepath)
+    return redirect(EMAIL_PAGE)
+
+
+@app.route("/", methods=["GET", "POST"])
 def index() -> Response:
     """The home page for the social media manager"""
     if request.method == "GET":
-        return render_template('index.html')
+        return render_template("index.html")
 
     is_success = True
 
     # Check if the content is empty
     if len(request.form.get(CONTENT_KEY, EMPTY_STRING)) == 0:
+        logging.error("Missing content")
         flash(EMPTY_CONTENT, ERROR_FLASH)
         return redirect(HOME_PAGE)
 
@@ -38,10 +156,11 @@ def index() -> Response:
     img_path = None
 
     if image is not None and image.filename != EMPTY_STRING:
+
         filename = secure_filename(image.filename)
         img_path = os.path.join(UPLOAD_FOLDER, filename)
+        logging.info(f"Image detected, saving to {img_path}")
         image.save(img_path)
-
 
     # Check if user picked at least one social media.
     to_post = set()
@@ -50,6 +169,7 @@ def index() -> Response:
             to_post.add(attr)
 
     if len(to_post) == 0:
+        logging.error("No social media selected")
         flash(SELECT_AT_LEAST_ONE_SOCIAL_MEDIA, ERROR_FLASH)
         return redirect(HOME_PAGE)
 
@@ -62,15 +182,20 @@ def index() -> Response:
                 message=request.form.get(CONTENT_KEY),
                 image=img_path,
             )
+            logging.info(f"Successfully posted to {post_type}")
         except Exception as e:
-            flash(f"Error posting to {post_type}: {e}", ERROR_FLASH)
+            msg = f"Error posting to {post_type}: {e}"
+            logging.error(msg)
+            flash(msg, ERROR_FLASH)
             is_success = False
-    
+
     # Remove the image after posting
     if img_path is not None:
         os.remove(img_path)
+        logging.info(f"Removed image {img_path}")
 
     if is_success:
+        logging.info(f"Successfully posted to {', '.join(to_post)}")
         flash(SUCCESS_MESSAGE, INFO_FLASH)
     return redirect(HOME_PAGE)
 
@@ -93,18 +218,48 @@ def create_default_config() -> None:
         "token": "",
         "group_id": "",
     }
-    with open(DEFAULT_CONFIG_FILE, 'w') as configfile:
+    config["email"] = {
+        "smtp_host": "",
+        "smtp_port": "",
+        "username": "",
+        "password": "",
+        "from_email": "",
+    }
+    with open(DEFAULT_CONFIG_FILE, "w") as configfile:
         config.write(configfile)
 
 
 if __name__ == "__main__":
+    logging.info("Starting server")
+
+    # Read Config File
     if not os.path.exists(DEFAULT_CONFIG_FILE):
         create_default_config()
+        logging.info(f"Created default configuration file at {DEFAULT_CONFIG_FILE}")
     credentials = configparser.ConfigParser()
     credentials.read(DEFAULT_CONFIG_FILE)
+    logging.info("Read configuration file")
+
+    # Setup email connection
+    logging.info("Setting up email connection to smtp server")
+    email_credentials = credentials["email"]
+    try:
+        email_server = EmailIntegration(
+            email_credentials["smtp_host"],
+            email_credentials["smtp_port"],
+            email_credentials["username"],
+            email_credentials["password"],
+            email_credentials["from_email"],
+        )
+    except Exception as e:
+        logging.error(f"Error starting email service: {e}")
+        raise e
 
     # Check if upload folder exists
     if not os.path.exists(UPLOAD_FOLDER):
+        logging.info(f"Creating upload folder: {UPLOAD_FOLDER}")
         os.mkdir(UPLOAD_FOLDER)
 
+    # Starting the app
+    logging.info("Starting the application")
     app.run(debug=False)
--- constants.py
+++ constants.py
@@ -1,25 +1,32 @@
-FACEBOOK = "facebook"
-TWITTER = "twitter"
-TELEGRAM = "telegram"
-
-SOCIAL_MEDIA = [
-    FACEBOOK,
-    TWITTER,
-    TELEGRAM
-]
-
+# Flask Message Flashes
 ERROR_FLASH = "error"
 INFO_FLASH = "info"
 
+# Page information
 HOME_PAGE = "/"
+EMAIL_PAGE = "/email"
+
+# For the social media main page
+FACEBOOK = "facebook"
+TWITTER = "twitter"
+TELEGRAM = "telegram"
+SOCIAL_MEDIA = [FACEBOOK, TWITTER, TELEGRAM]
 
 CONTENT_KEY = "content"
 IMAGE_KEY = "image"
 EMPTY_STRING = ""
 UPLOAD_FOLDER = "uploads"
 
+# Constants for the email page
+SUBJECT_KEY = "subject"
+BODY_KEY = "body"
+CSV_KEY = "csv"
+EMAIL_KEY = "email"
+
+# Config File
 DEFAULT_CONFIG_FILE = "config.ini"
 
+# Error messages
 EMPTY_CONTENT = "No content provided"
 SELECT_AT_LEAST_ONE_SOCIAL_MEDIA = "Please select at least 1 social media"
 SUCCESS_MESSAGE = "Message will be sent soon"
